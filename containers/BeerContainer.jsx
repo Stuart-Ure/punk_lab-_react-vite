79590cb01fe75c83e89426a30dc8d16e7a535900
Stuart <stuartrure@gmail.com>
2023-08-26 09:54:35 +0100

    more functionality added

--- containers/BeerContainer.jsx
+++ containers/BeerContainer.jsx
@@ -1,25 +1,56 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const BeerContainer = () => {
-const [beers, setBeers] = useState([]);
-const [selectedBeer, setSelectedBeer] = useState(null);
-const [favouriteBeers, setFavouriteBeers]= useState([]);
+    const [beers, setBeers] = useState([]);
+    const [selectedBeer, setSelectedBeer] = useState(null);
+    const [favouriteBeers, setFavouriteBeers] = useState([]);
+    const [randomBeer, setRandomBeer] = useState(null);
 
+    useEffect(() => {
+        fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
+            .then(response => response.json())
+            .then(data => setBeers(data));
+    }, []);
 
-useEffect(() => {
-    fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
-    .then(response => response.json())
-    .then(data => setBeers(data));
-}, []);
+    const fetchRandomBeer = () => {
+        fetch('https://api.punkapi.com/v2/beers/random')
+            .then(response => response.json())
+            .then(data => setRandomBeer(data[0]));
+    };
 
-return (
-    <>
-    <div className='title'> 
-    <Title title="Beers" />
-    </div>
-    <BeersList beers={beers} selectedBeer={selectedBeer} setSelectedBeer={setSelectedBeer} favouriteBeers={favouriteBeers} setFavouriteBeers={setFavouriteBeers}/>
-    </>
-);
+    const toggleFavouriteRandomBeer = () => {
+        if (randomBeer) {
+            const isFavourite = favouriteBeers.some(favouriteBeer => favouriteBeer.id === randomBeer.id);
+            const updatedFavourites = isFavourite
+                ? favouriteBeers.filter(favouriteBeer => favouriteBeer.id !== randomBeer.id)
+                : [...favouriteBeers, randomBeer];
+
+            setFavouriteBeers(updatedFavourites);
+        }
+    };
+
+    return (
+        <>
+            <div className='title'>
+                <Title title="Beers" />
+                <button onClick={fetchRandomBeer}>Random Beer</button>
+            </div>
+            {randomBeer && (
+                <div>
+                    <h2>Random Beer</h2>
+                    <h3>{randomBeer.name}</h3>
+                    <h4>{randomBeer.tagline}</h4>
+                    <p>{randomBeer.description}</p>
+                    <h4>{randomBeer.abv} %</h4>
+                    <button onClick={toggleFavouriteRandomBeer}>
+                        {favouriteBeers.some(favouriteBeer => favouriteBeer.id === randomBeer.id) ? "Remove from Favourites" : "Add to Favourites"}
+                    </button>
+                </div>
+            )}
+            <hr />
+            <BeersList beers={beers} selectedBeer={selectedBeer} setSelectedBeer={setSelectedBeer} favouriteBeers={favouriteBeers} setFavouriteBeers={setFavouriteBeers} />
+        </>
+    );
 };
 
 const Title = ({ title }) => {
@@ -48,7 +79,7 @@ const BeersList = ({ beers, selectedBeer, setSelectedBeer, favouriteBeers, setFa
             <ul className='fav--list'>
                 {favouriteBeers.map((favouriteBeer) => (
                     <li key={favouriteBeer.id}>
-                        {favouriteBeer.name} - {favouriteBeer.tagline}
+                        {favouriteBeer.name} - {favouriteBeer.tagline} -{favouriteBeer.abv} % - {}
                     </li>
                 ))}
             </ul>
@@ -73,11 +104,12 @@ const Beer = ({ beer, selectedBeer, setSelectedBeer, favouriteBeers, setFavourit
     <>
         <li className='selectfav'>
         <h2 onClick={() => setSelectedBeer(beer)}> {beer.name}</h2>
-        <button onClick={toggleFavourite}>
-            {isFavourite ? "Remove from Favourites" : "Add to Favourites"}
-        </button>
         <h3>{beer.tagline}</h3>
-        {selectedBeer&& selectedBeer.id == beer.id && <h4>{beer.description} </h4>}
+        {selectedBeer&& selectedBeer.id == beer.id && <h4>{beer.description}</h4>}
+        <h4> {beer.abv} %</h4>  
+        <button onClick={toggleFavourite}>
+        {isFavourite ? "Remove from Favourites" : "Add to Favourites"}
+        </button>
         </li>
         <hr />
     </>
